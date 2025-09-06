56f87405a83ceaa51a1423897b5e0991370ce462
Lee Dayeon <carrotday0429@gmail.com>
2025-09-06 22:46:12 +0900

    chore #3 - 채팅 페이지 생성

---
+++ src/pages/chat/ChatDetailPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home-page">
+    <section class="welcome">
+      <h1 class="titleExtra24px">채팅 디테일 페이지</h1>
+      <p class="bodyRegular16px">
+        여기는 1:1 채팅 화면이야.
+      </p>
+    </section>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.home-page {
+  padding: 16px;
+}
+
+.welcome {
+  text-align: center;
+  margin-top: 40px;
+}
+</style>
