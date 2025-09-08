407020daf64b7a57034aa9f1f74b52cf06eb06df
SuyeonShinnnn <suyeons5536@gmail.com>
2025-09-08 19:53:52 +0900

    feat #4 - 채팅 입력 및 전송 컴포넌트 구현

---
+++ src/pages/chat/components/ChatInput.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="input-wrapper">
+    <div class="btn"><img src="@/assets/icons/chat/plus.png" /></div>
+    <input
+      v-model="textInput"
+      @keyup.enter="sendMessage"
+      class="text-input"
+      placeholder="메시지를 입력하세요"
+    />
+    <div class="btn" @click="sendMessage">
+      <img src="@/assets/icons/chat/send.png" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const textInput = ref('');
+const emit = defineEmits(['send']);
+
+const sendMessage = () => {
+  if (!textInput.value.trim()) return;
+  emit('send', textInput.value);
+  textInput.value = '';
+};
+</script>
+
+<style scoped>
+.input-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  gap: 0.7rem;
+  padding: 0.5rem 0.8rem;
+  border-top: 1px solid var(--color-lightgray);
+  background-color: var(--color-white);
+
+  width: 100%;
+  box-sizing: border-box;
+}
+
+.btn {
+  width: 20px;
+}
+
+.btn > img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+
+.text-input {
+  height: 30px;
+  background-color: #f2f3f7;
+  border: none;
+  border-radius: 50px;
+  padding: 1.5rem 1rem;
+  width: 80%;
+}
+</style>
