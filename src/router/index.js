fbf1c8fa692c541bba0e9403f2589016dc1dde77
SuyeonShinnnn <suyeons5536@gmail.com>
2025-09-10 16:40:59 +0900

    chore #20 - 채팅 라우트 파일 분리

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomePage from '@/pages/home/HomePage.vue'
-import NotFoundPage from '@/pages/home/NotFoundPage.vue'
-import authRoutes from './auth.js'
-import listingRoutes from './listing.js'
-import ChatDetailPage from '@/pages/chat/ChatDetailPage.vue'
-import ChatListPage from '@/pages/chat/ChatListPage.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '@/pages/home/HomePage.vue';
+import NotFoundPage from '@/pages/home/NotFoundPage.vue';
+import authRoutes from './auth.js';
+import listingRoutes from './listing.js';
+import chatRoutes from './chat.js';
 
 const routes = [
   {
@@ -14,34 +13,18 @@ const routes = [
   },
   ...authRoutes,
   ...listingRoutes,
-  {
-    path: '/chat-detail/:id',
-    name: 'chat-detail',
-    component: ChatDetailPage,
-    meta: {
-      hideHeader: true,
-      hideFooter: true,
-    },
-  },
-  {
-    path: '/chat-list',
-    name: 'chat-list',
-    component: ChatListPage,
-    meta: {
-      hideHeader: true,
-    },
-  },
+  ...chatRoutes,
   {
     // 정의되지 않은 모든 경로 처리
     path: '/:pathMatch(.*)*',
     name: 'not-found',
     component: NotFoundPage,
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-})
+});
 
-export default router
+export default router;
