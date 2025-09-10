03ae42313a01a4b90bf0061e188ee980fbaaf19b
SuyeonShinnnn <suyeons5536@gmail.com>
2025-09-11 00:00:53 +0900

    chore #26 - 매물 정보 폰트 크기 수정

--- src/pages/chat/components/MarketInfo.vue
+++ src/pages/chat/components/MarketInfo.vue
@@ -2,8 +2,10 @@
   <div class="container">
     <div class="market-img"></div>
     <div class="market-info">
-      <label class="market-name">한담 해안도로 카페거리 카페 24평</label>
-      <label class="titleExtra16px">매매 1억 2천</label>
+      <label class="bodyMedium14px market-name"
+        >한담 해안도로 카페거리 카페 24평</label
+      >
+      <label class="bodyBold14px">매매 1억 2천</label>
     </div>
   </div>
 </template>
@@ -20,8 +22,8 @@
 }
 
 .market-img {
-  width: 55px;
-  height: 55px;
+  width: 50px;
+  height: 50px;
   background-color: black;
   border-radius: 10px;
 }
