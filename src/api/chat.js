04680ce46cea2c99ed00911c121d1656c7c1a9fa
SuyeonShinnnn <suyeons5536@gmail.com>
2025-09-11 16:02:53 +0900

    fix #43 - 경로 수정

--- src/api/chat.js
+++ src/api/chat.js
@@ -2,10 +2,22 @@ import axios from 'axios';
 
 export const getChatRoom = async (userId) => {
   try {
-    const res = await axios.get(`/chat/rooms/${userId}`);
+    const res = await axios.get(`/chat/main/${userId}`);
     console.log(res.data);
     return res.data;
   } catch (err) {
     console.log(err);
   }
 };
+
+// export const getChatDetail = async (chatRoom) => {
+//   const id = 1;
+//   try {
+//     // 채팅 내용 불러오기
+//     const res = await axios.get(`/chat/main/${id}`);
+//     console.log(res.data);
+//     return res.data;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
